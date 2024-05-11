3ff388024e7057701aec258e1dbece069455e44f
Liam Kalir <liam.kalir@protonmail.com>
2024-05-11 19:16:50 -0400

    Fixed typo

--- zellij-zsh-completions.plugin.zsh
+++ zellij-zsh-completions.plugin.zsh
@@ -4,7 +4,7 @@ fi
 
 # If the completion file doesn't exist yet, we need to autoload it and
 # bind it to `zellij`. Otherwise, compinit will have already done that
-if [[ ! -f "ZSH_CACHE_DIR/completions/_zellij" ]]; then
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_zellij" ]]; then
   autoload -Uz _zellij
   typeset -g -A _comps
   _comps[zellij]=_zellij
